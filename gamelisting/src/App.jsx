dc8e5d5b83733adf449e3b4b804392f6aacc8aef
MatthewOfomi <ofomimatthew7@gmail.com>
2024-02-16 18:40:49 +0100

    first commit

---
+++ gamelisting/src/App.jsx
@@ -0,0 +1,12 @@
+import Home from "./Pages/Home"
+import Header from "./Components/Header"
+
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+    <Header />
+    <Home />
+    </h1>
+  )
+}
